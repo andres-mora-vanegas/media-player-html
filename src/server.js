925b908823ac042d506700e65f09c1f977163c4f
andres-mora-vanegas1 <andresmoradeveloper@gmail.com>
2020-03-07 19:33:57 -0500

    [update]add ipconfigs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const path = require("path");
+const os = require("os");
+const ifaces = os.networkInterfaces();
 
 function recFindByExt(base, ext, files, result) {
   files = files || fs.readdirSync(base);
@@ -20,7 +22,6 @@ function recFindByExt(base, ext, files, result) {
 
 function getMovies() {
   try {
-    //   const rooti= fs.readdirSync('/Downloads/peliculas');
     let response = [];
     response.push(recFindByExt("/Downloads/peliculas", "mp4"));
     if (response.length >= 1) {
@@ -28,24 +29,20 @@ function getMovies() {
     }
     response = response[0].concat(response[1]);
     let lis = ``;
-    const preg= new RegExp(`\\Downloads`,'g');
-    const back= new RegExp(`\/`,'g');
     response.forEach(element => {
       const spliti = element.split("\\");
       const last = spliti.length - 1;
-      console.log(`element`,element);
-      console.log(`preg.test(element)`,preg.test(element));
-      let realLink = element.replace(/\\/g,"/");
-      // realLink = element.replace(/\/Downloads\/peliculas/g,"http://127.0.0.1/peliculas/");
-      // realLink = element.replace(back,'/');
+      let realLink = element.replace(/\\/g, "/");
       lis += `<li><a href="${realLink}">${spliti[last]}</a></li>`;
     });
+
+    const ip = ifaces["Wi-Fi"].find(el => el.family == "IPv4").address;
+
+    lis = lis.replace(/\/Downloads/g, "http://" + ip);
 
     let ind = fs.readFileSync("./src/template.html", "utf8");
     ind = ind.replace("#movie-list#", lis);
     fs.writeFileSync("./src/index.html", ind, "utf8");
-
-    // console.log(`rooti`, response);
   } catch (error) {
     handleError(error);
   }
